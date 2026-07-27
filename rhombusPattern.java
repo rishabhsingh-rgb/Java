ea1f18e5ff3ae71e246c8fd694eadc49a961b359
Rishabh Singh <rishabhsingh742006@gmail.com>
2026-07-27 21:36:36 +0530

    Added a rhombus pattern printing for desired number of rows

---
+++ rhombusPattern.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class rhombusPattern{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int row=sc.nextInt();
+        for(int i=1;i<=row;i++){
+            for(int j=0;j<row-i;j++){
+                System.out.print("  ");
+            }
+            for(int k=1;k<=row;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
