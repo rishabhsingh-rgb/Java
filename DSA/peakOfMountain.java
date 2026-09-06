7e83ff5699ad316e208ed16192a1ba8cb8911e23
Rishabh Singh <rishabhsingh742006@gmail.com>
2026-09-07 02:13:30 +0530

    Implement peak index finder in mountain array

---
+++ DSA/peakOfMountain.java
@@ -0,0 +1,19 @@
+class peakOfMountain {
+    public int peakIndexInMountainArray(int[] arr) {
+        int n=arr.length;
+        int low=0;
+        int high=n-1;
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>=arr[mid+1]){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+}
